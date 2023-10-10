ac7301f5e055ab9caa41d67ab0e7c5083d782a43
Ankitsinghchouhan <143715256+Ankitsinghchouhan@users.noreply.github.com>
2023-10-10 21:05:14 +0530

    Add files via upload

---
+++ Parent.java
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+class Parent{
+void display(){
+    System.out.println("This is the parent class");
+}
+void show(int num){
+    System.out.println("The number is the parenrt class is:"+num);
+}
+}
+class child extends Parent{
+void.display()
+{
+    System.out.println("this is a child class");
+}
+void show(double num)
+{
+    System.out.println("the no is the child class is"+num);
+}
+}
+public class Main{
+    public static vopid main(String[]args)
+    {
+        parent parentobj=new Parent();
+        parent childObj=new Parent();
+        parentObj.show(s);
+        childObj.show(7.5);
+        parentOnj.display();
+        childObj.display();
+    }
+}
+ 
+  
+   
